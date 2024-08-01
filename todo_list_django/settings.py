9ff0b40eb7e2d80fa1dba24e608edc9a94ddf601
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-01 16:04:55 +0300

    Add AUTH_USER_MODEL reference and LOGIN_REDIRECT_URL to the settings.py

--- todo_list_django/settings.py
+++ todo_list_django/settings.py
@@ -106,6 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "todos.AppUser"
+
+LOGIN_REDIRECT_URL = "/"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
