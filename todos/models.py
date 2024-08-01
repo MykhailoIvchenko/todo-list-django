b189a8e7b1fb08501c9f485f932fcdddd26a1104
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-01 12:55:40 +0300

    Add user model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("todos:user-detail", kwargs={"pk": self.pk})
