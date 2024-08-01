7d834a38b7a7232be2674ead643684cf5b359025
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-01 14:58:45 +0300

    Add TagListView for tags displaying

--- todos/views.py
+++ todos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
-from todos.models import Task
+from todos.models import Task, Tag
 
 
 class IndexView(LoginRequiredMixin, generic.ListView):
@@ -12,3 +12,10 @@ class IndexView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user).select_related("tags")
+
+
+class TagListView(LoginRequiredMixin, generic.ListView):
+    model = Tag
+    context_object_name = "tags_list"
+    template_name = "todos/tags_list.html"
+    paginate_by = 5
