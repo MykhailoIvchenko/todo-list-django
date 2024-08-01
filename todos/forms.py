98e49d2fd845332173ce6272312faf57e6fb6dd2
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-01 14:47:46 +0300

    Add TaskCreateFrom for task creation and update

--- todos/forms.py
+++ todos/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-from todos.models import AppUser
+from todos.models import AppUser, Task, Tag
 
 
 class AppUserForm(UserCreationForm):
@@ -11,3 +11,14 @@ class AppUserForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + (
             "first_name", "last_name")
 
+
+class TaskCreateForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
