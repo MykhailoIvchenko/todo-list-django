87c60e6a21cbefcd138864347053ae6622d14886
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-01 14:55:37 +0300

    Add view for the home page with tasks list

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 
-# Create your views here.
+from todos.models import Task
+
+
+class IndexView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    paginate_by = 5
+    context_object_name = "tasks_list"
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user).select_related("tags")
